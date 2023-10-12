a4891079ab450d064ab1a6dbe94e1e053b1f2cab
inguyent <138817868+inguyent@users.noreply.github.com>
2023-10-12 12:43:10 -0700

    Update and rename hw1pt2b.m to hw1_pt2b.m

--- hw1_pt2b.m
+++ hw1_pt2b.m
@@ -2,7 +2,7 @@ a = RR_poly([1 0 -46 0 369 0 -324]);
 b = RR_poly([1 0 -29 0 100]);
 k = 6;
 % Exponential k was increased until the Diophantine function gave a proper D(s)
-f = RR_poly([1 20 154 576 1089 972 324]) * (RR_poly([1 20]))^6 ;
+f = RR_poly([1 20 154 576 1089 972 324]) * (RR_poly([1 20]))^k ;
 % RR_diophantine solves for the Diophantine equation and gives x and y where D(s) = y/x
 [x,y] = RR_diophantine(a,b,f)
 % Test to confirm D(s)
