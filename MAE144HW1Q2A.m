a99ccf29db9888b460443cffa54955169c8773c1
inguyent <138817868+inguyent@users.noreply.github.com>
2023-10-12 11:39:46 -0700

    Add files via upload

---
+++ MAE144HW1Q2A.m
@@ -0,0 +1,8 @@
+a = RR_poly([1 0 -46 0 369 0 -324])
+b = RR_poly([1 0 -29 0 100])
+f = RR_poly([1 20 154 576 1089 972 324])
+[x,y] = RR_diophantine(a,b,f)
+%Test to confirm D(s)
+test=trim(a*x+b*y)
+residual=norm(f-test)
+%Note that a*x + b*y = f has ~0 residial which confirms D(s)
