f2e255e54884f62652455bd3078dd3a2f8544e78
inguyent <138817868+inguyent@users.noreply.github.com>
2023-10-12 12:56:45 -0700

    Update hw1_pt2a.m

--- hw1_pt2a.m
+++ hw1_pt2a.m
@@ -1,6 +1,6 @@
-a = RR_poly([1 0 -46 0 369 0 -324])
-b = RR_poly([1 0 -29 0 100])
-f = RR_poly([1 20 154 576 1089 972 324])
+a = RR_poly([1 0 -46 0 369 0 -324]);
+b = RR_poly([1 0 -29 0 100]);
+f = RR_poly([1 20 154 576 1089 972 324]);
 % RR_diophantine solves for the Diophantine equation and gives x and y where D(s) = y/x
 [x,y] = RR_diophantine(a,b,f)
 % Test to confirm D(s)
